2baa7b9f0930d47526e18ce0716dfac380ca29a1
achrefmechergui <achreffcb123achref@gamil.com>
2024-11-01 20:49:32 +0100

    pt2

--- app.js
+++ app.js
@@ -10,11 +10,20 @@
 
 // console.log(document.querySelector(".guess").value)
 
+//  console.log(document.querySelector(".between").textContent)  
+
+
+
   const secretNumber = Math.trunc(Math.random()*20+1)
 
-  document.querySelector('.head-icon').textContent = secretNumber
+//   document.querySelector('.head-icon').textContent = secretNumber
 
-document.querySelector(".check").addEventListener("click",function(){
+
+  let score = 20 ; 
+
+     /// dry :  d'ont repeat your self 
+
+   document.querySelector(".check").addEventListener("click",function(){
     const guess = Number(document.querySelector(".guess").value)
     if (!guess){
      document.querySelector(".start").textContent = " ⛔ No Number !!! "
@@ -22,12 +31,37 @@ document.querySelector(".check").addEventListener("click",function(){
 
     if (guess === secretNumber){
         document.querySelector(".start").textContent = "🎉 Correct number !!! "
+        score ++ 
+        document.querySelector(".score-result").textContent = score 
+        document.querySelector("body").style.backgroundColor = "greenyellow"
+        document.querySelector('.head-icon').style.padding = "15px 65px"
+     
     }
 
-    if (guess>secretNumber){
-        document.querySelector(".start").textContent = " 📈 too high !!!"
-    }
+
+
+    if (score > 0 ){
+        if (guess>secretNumber){
+            document.querySelector(".start").textContent = " 📈 too high !!!"
+            score --
+            document.querySelector(".score-result").textContent = score 
+        }
+    } 
+
+
+
+
+
+  if (score > 0 ){
     if (guess && guess<secretNumber){
         document.querySelector(".start").textContent = " 📉 too low !!!"
+        score --
+        document.querySelector(".score-result").textContent = score 
     }
+  }
+  else {
+    document.querySelector(".start").textContent = " 💥 you loose !!!"
+}
+
+
 })
